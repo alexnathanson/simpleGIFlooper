a9c20526e4412b92fc54dabc55bd31c5cd84fd89
alexnathanson <alex@alexnathanson.com>
2018-12-23 17:37:13 -0500

    'files'

--- giflooper.py
+++ giflooper.py
@@ -1,4 +1,19 @@
 from os import system
+from os import listdir
 
 
 system('convert -delay 10 -loop 0 image*.jpg test.gif')
+
+path = 'media'
+
+vidType = 'mp4'
+
+files = os.listdir(path)
+
+vids = []
+
+for name in files:
+	if vidType in name:
+		vids.append(name)
+
+print(vids)
